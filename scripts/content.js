f0f32a3631fb785cc0e6cf01dbfc6022d75c6ab3
osorosia <64416398+osorosia@users.noreply.github.com>
2023-08-29 02:18:24 +0900

    push

--- scripts/content.js
+++ scripts/content.js
@@ -8,15 +8,22 @@ if (taskStatement) {
   var matches = url.match(regex);
   var contestName = matches[1];
   var problemName = matches[2];
-  var destURL = `https://atcoder.jp/contests/${contestName}/submissions?f.LanguageName=Rust&f.Status=AC&f.User=&orderBy=created&f.Task=${problemName}`;
 
-  // Create button
-  const button = document.createElement("button");
-  button.innerHTML = "AC List";
-  button.onclick = () => {
-    location.href = destURL;
+  const createButton = (lang, buttonName) => {
+    const destURL = `https://atcoder.jp/contests/${contestName}/submissions?f.LanguageName=${lang}&f.Status=AC&f.User=&orderBy=created&f.Task=${problemName}`;
+    const button = document.createElement("button");
+    button.innerHTML = buttonName ?? lang;
+    button.onclick = () => {
+      location.href = destURL;
+    };
+    return button;
   };
 
+  // Create button
+  const buttonRust = createButton("Rust");
+  const buttonFSharp = createButton("F%23", "F#");
+
   // Add button
-  taskStatement.insertAdjacentElement("beforebegin", button);
+  taskStatement.insertAdjacentElement("beforebegin", buttonRust);
+  taskStatement.insertAdjacentElement("beforebegin", buttonFSharp);
 }
